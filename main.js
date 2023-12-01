00609edefc50ddb0211d85b95ac6bc8f6bd1598b
Wadie Benabdouh <wadie.trade@gmail.com>
2023-12-02 00:26:59 +0100

    main

--- main.js
+++ main.js
@@ -18,4 +18,27 @@ accordionBtnOne.onclick = () => {
 }
 //NOS OFFRES END
 
+//Nos centres d'expertises
+let accordionBtnTwo = document.getElementById("accordionBtn-two");
+let innerTwo = document.getElementById("inner-two")
+let caretTwo = document.getElementById("caret-two")
+
+accordionBtnTwo.onclick = () => {
+    innerTwo.classList.toggle("is-open");
+    caretTwo.classList.toggle("active");
+}
+//Nos centres d'expertises END
+
+// Nos Partenaires
+let accordionBtnThree = document.getElementById("accordionBtn-three");
+let innerThree = document.getElementById("inner-three");
+let caretThree = document.getElementById("caret-three")
+
+accordionBtnThree.onclick = () => {
+    innerThree.classList.toggle("is-open")
+    caretThree.classList.toggle("active");
+}
+
+// Nos Partenaires END
+
 //---------MOBILE NAV LIST---------//
