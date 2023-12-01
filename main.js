fd58e212c0af6c5e758c3955e6b1d4437374cdec
Wadie Benabdouh <wadie.trade@gmail.com>
2023-12-01 23:57:26 +0100

    MAIN

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ mobileNav.onclick = () => {
     mobileNav.classList.toggle("is-active")
 }
 
-//MOBILE NAV LIST 
+//---------MOBILE NAV LIST---------//
+
+//NOS OFFRES
 let accordionBtnOne = document.getElementById("accordionBtn-one");
 let innerOne = document.getElementById("inner-one");
+let caretOne = document.getElementById("caret-one")
 
 accordionBtnOne.onclick = () => {
     innerOne.classList.toggle("is-open")
+    caretOne.classList.toggle("active")
 }
+//NOS OFFRES END
+
+//---------MOBILE NAV LIST---------//
